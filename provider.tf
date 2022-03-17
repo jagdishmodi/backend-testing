f3d0426396e96c7af474777b5eaf6d756330c0bb
Jagdish <learnitsolutions1983@gmail.com>
2022-03-17 13:06:27 +0530

    net

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+ backend "s3" {
+   region = "ap-south-1"
+   bucket = "backendbucket2022"
+   key = "terraform-trainer/terraform.tfstate2"
+ }
+  
 }
 
 provider "aws" {
--- resource.tf
+++ resource.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "foo" {
   ami           = "ami-04893cdb768d0f9ee" 
   instance_type = "t2.micro"
    tags = {
-       Name = "hello-World_test"
+       Name = "hello-World"
    }
   
   }
